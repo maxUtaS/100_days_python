1aa68b3436401e9b363a696d20a28db57260eb88
MaximBerlin <skakun_maxim@gmx.de>
2021-01-23 03:49:54 +0100

    creat a exercise 2.1

---
+++ day_02/exercise_2_1.py
@@ -0,0 +1,10 @@
+two_digit_number = input("Type a two digit number: ")
+
+# get first and second digits and convert string to int
+first_number = int(two_digit_number[0])
+second_number = int(two_digit_number[1])
+
+# add two digits together
+result = first_number + second_number
+
+print(result)
